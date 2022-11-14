f8532ecc937bc0628397976137c33db3ef6563a3
oash69 <recchia.nicolas@gmail.com>
2022-11-14 12:58:12 +0100

    searche memeber

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, SimpleChanges, OnChanges, Input } from '@angular/core';
 import { MemberService } from './member.service';
 import { OnInit } from '@angular/core';
 import { Membre } from './membre';
+
 
 @Component({
   selector: 'app-root',
@@ -12,18 +13,34 @@ import { Membre } from './membre';
 export class AppComponent implements OnInit {
   title = 'member-frontend';
   member :Membre;
-
+  lastname : string;
+  firstname: string;
+  modified:boolean = false;
 
   constructor(private memberService: MemberService){}
 
   ngOnInit(): void {
       console.log('On Init ...')
-      this.memberService.getMember().subscribe (response => {this.member = response;});
+     // this.memberService.getMember().subscribe (response => {this.member = response;});
+  }
+
+  ngOnChanges(changements: SimpleChanges) {
+    //Insérez votre code de détection du changement ici
+    console.log(changements); //Valeur actuelle du libellé (après le changement)
+  }
+
+  onChangePhone(){
+    console.log(this.member.telephone) ;
   }
 
   onSave():void{
     console.log('On save ...')
-    this.memberService.updateMember(this.member);
+    this.memberService.updateMember(this.member).subscribe (response => {this.member = response;});
     console.log('On save after ...')
+    this.modified=true;
+  }
+
+  onChangeMember(){
+    this.memberService.getMemberByFirstnameAndLastName(this.firstname,this.lastname).subscribe (response => {this.member = response;});
   }
 }
--- src/app/member.service.ts
+++ src/app/member.service.ts
@@ -10,8 +10,9 @@ export class MemberService {
 
   readonly API_URL="http://localhost:8080"
   readonly ENDPOINT_MEMBER_READ = "/bali/services/v1/memberFacade?firstName=NORBERT&lastName=HEUZER"
+  readonly ENDPOINT_MEMBER_READ_FIRST_LAST = "/bali/services/v1/memberFacade?firstName="
   readonly ENDPOINT_MEMBER_READ2 = "/bali/services/v1/memberPhoneChange?tel="
-  readonly ENDPOINT_MEMBER_UPDATE = "/bali/services/v1/"
+  readonly ENDPOINT_MEMBER_UPDATE = "/bali/services/v1/putChange"
 
   constructor(private httpClient : HttpClient) { 
 
@@ -21,9 +22,16 @@ export class MemberService {
     return this.httpClient.get<Membre>(this.API_URL+this.ENDPOINT_MEMBER_READ)
   }
 
+  public getMemberByFirstnameAndLastName(firstname:string,lastname:string): Observable<Membre>{
+    console.log(this.API_URL+this.ENDPOINT_MEMBER_READ_FIRST_LAST + firstname + '&='+lastname)
+    return this.httpClient.get<Membre>(this.API_URL+this.ENDPOINT_MEMBER_READ_FIRST_LAST + firstname + '&lastName='+lastname)
+  }
+
   public updateMember(membre :Membre): Observable<Membre>{
     console.log('update member ...' + membre.id + " " + membre.telephone)
-    //return this.httpClient.put<Membre>(this.API_URL+this.ENDPOINT_MEMBER_UPDATE+membre.id,membre)
+    /*console.log('lien: ' + this.API_URL+this.ENDPOINT_MEMBER_UPDATE+membre.telephone +  "&=" + membre.id)
+    return this.httpClient.put<Membre>(this.API_URL+this.ENDPOINT_MEMBER_UPDATE+membre.telephone +  "&=" + membre.id)*/
+    console.log('lien: ' + this.API_URL+this.ENDPOINT_MEMBER_READ2+ membre.telephone + "&id=" + membre.id)
     return this.httpClient.get<Membre>(this.API_URL+this.ENDPOINT_MEMBER_READ2+ membre.telephone + "&id=" + membre.id)
     console.log('after put')
   }
